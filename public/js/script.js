ee4af1a69d0aef6ea705e8090f1685cab78c9356
Sorin <sorin_socy_89@yahoo.com>
2017-12-23 01:41:44 +0200

    prima versiune a proiectului

---
+++ public/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.galerie').lightSlider({
+        gallery:true,
+        item:1,
+        loop:true,
+        slideMargin:0,
+        enableDrag: false,
+        currentPagerPosition:'left',
+        pager: false,
+        auto: true
+    });  
+});
